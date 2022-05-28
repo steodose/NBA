177a31425d9a40b5c9d6168acf0a59b29c63e5bd
Stephan Teodosescu <32054537+steodose@users.noreply.github.com>
2022-05-27 20:50:26 -0700

    Updated NBA playoffs suck graphic

--- NBA Playoffs Suck.R
+++ NBA Playoffs Suck.R
@@ -99,13 +99,13 @@ add_logo <- function(plot_path, logo_path, logo_position, logo_scale = 10){
 
 ##### Data wrangling #####
 
-# Read in games data from nbastatR
+# Read in games and results data from nbastatR
 
 nba_games <- nbastatR::current_schedule() #this gets the current season's schedule and results
 
 
 # Get game IDs for Playoff games going back to 2010 for longer analysis, to be joined later
-selectedSeasons <- c(2010:2022)
+selectedSeasons <- c(2000:2022)
 
 # get the current playoff schedule
 playoffs <- nbastatR::seasons_schedule(seasons = selectedSeasons, season_types = "Playoffs")
@@ -118,32 +118,79 @@ playoffs_gamelog <- game_logs(seasons = selectedSeasons, league = "NBA",
 
 
 # Get win probability and play by play data for each playoff game
-wp <- playoffs %>% 
-    pull(idGame) %>% 
-    nbastatR::win_probability()
-
-
+#wp <- playoffs %>% 
+ #   pull(idGame) %>% 
+  #  nbastatR::win_probability()
 
 
 ##### Data Visualization #####
 
-## stacked chicklet chart
+## 1. stacked chicklet chart
 
 # join playoffs and nba games datasets
 playoffs2 <- playoffs %>% 
-    left_join(nba_games) %>% 
-    mutate(point_differential = abs(scoreHome - scoreAway)) %>% 
+    left_join(playoffs_gamelog) %>% 
+    mutate(point_differential = abs(plusminusTeam)) %>% 
     mutate(quality = case_when(
         point_differential >= 20 ~ "Blowout",
         point_differential >= 10 & point_differential < 20 ~ "Meh",
-        TRUE ~ "Close"
+        TRUE ~ "Good"
     ))
 
-
+# finalize dataframe for our chart
 blowout_df <- playoffs2 %>% 
     group_by(slugSeason) %>% 
     count(quality) %>% 
-    mutate(share = n/sum(n))
+    mutate(share = n/sum(n)) %>% 
+    mutate(quality = fct_relevel(quality, c("Blowout", "Meh", "Good")))
 
 
+blowouts <- blowout_df %>% 
+    ggplot(aes(slugSeason, share)) + 
+    geom_chicklet(aes(fill = quality)) +
+    geom_text(data = . %>% 
+                  filter(share > .1), aes(label = percent(share, accuracy = 1L)), 
+              family = "Outfit", size = 3, color = "white", fontface = "bold", position = position_stack(vjust = 0.5)) +
+    theme_custom_floralwhite() +
+    theme(axis.text.x = element_text(angle=90, hjust=1)) +
+    theme(legend.position = 'top',
+          legend.title = element_blank(),
+          axis.title.y=element_blank(),
+          axis.text.y=element_blank(),
+          axis.text.x = element_text(angle = 45),
+          plot.title = element_text(face = 'bold', size = 16, hjust = .5), 
+          plot.subtitle = element_text(size = 10, hjust = .5), 
+          plot.caption = element_text(size = 10)
+    ) + 
+    labs(x = "",
+         y = "",
+         title = "NBA Playoff Basketball Kinda Sucks", 
+         subtitle = "Blowout defined as % of games ending with 20+ point margin; Meh games = 10-19 points; Good games = 0-9 points.",
+         caption = "Data: nbastatR | Graphic: @steodosescu") +
+    guides(fill=guide_legend(
+        keywidth= .5,
+        keyheight= .2,
+        default.unit="inch", 
+        label.position = 'top', 
+        nrow = 1) 
+    ) +
+    scale_fill_manual(values = c("#C9082A", "#a9a9a9", "#17408B"))
+
+blowouts
+
+ggsave("Blowouts Chicklet Chart.png")
+
+
+# Add NBA logo to plot
+
+blowouts_chicklet_logo <- add_logo(
+    plot_path = "/Users/Stephan/Desktop/R Projects/NBA/Blowouts Chicklet Chart.png", # url or local file for the plot
+    logo_path = "/Users/Stephan/Desktop/R Projects/NBA/2021/nba-logo.png", # url or local file for the logo
+    logo_position = "top left", # choose a corner
+    # 'top left', 'top right', 'bottom left' or 'bottom right'
+    logo_scale = 30
+)
+
+# save the image and write to working directory
+magick::image_write(blowouts_chicklet_logo, "Blowouts Chicklet Chart with Logo.png")
 
